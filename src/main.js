f29f7a75a511d3396cd0169b3e7394ed386e98f9
Lyza Stevens <91556557+lyza13@users.noreply.github.com>
2022-04-18 14:53:49 -0700

    add comments w/ point breakdown

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+
+// TITLE: Bubble Patrol
+// NAME: Lyza Stevens
+// DATE: 4.18.2022
+// TIME TO COMPLETE: 20 hours
+
+// ------- POINT BREAKDOWN -------
+// ****might include some double dippage, so I went over 100 total points just in case****
+
+// (60) Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)
+// (20) Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
+// (10) Implement parallax scrolling 
+// (10) Create a new animated sprite for the Spaceship enemies 
+// (10) Display the time remaining (in seconds) on the screen 
+// (10) Create a new title screen (e.g., new artwork, typography, layout)
+// (5)  Allow the player to control the Rocket after it's fired
+// (5)  Add your own (copyright-free) background music to the Play scene
+
+// (5) Implement the speed increase that happens after 30 seconds in the original game
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -12,3 +32,4 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
